949d9fdf51a28e723c796164b0e7edf72940b0ea
yathavan <yadhavanprabhakar744@gmail.com>
2021-02-07 11:01:19 +0530

    change

--- src/Components/pages/HomepageFiles/Form.js
+++ src/Components/pages/HomepageFiles/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Form, Formik, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 
-function FormComp({state}) {
+function FormComp({ state }) {
   const validationSchema = yup.object().shape({
     name: yup.string().required(" name is required"),
     email: yup
@@ -15,7 +15,7 @@ function FormComp({state}) {
     name: "",
     email: "",
     comments: "",
-    files:'',
+    files: "",
   };
 
   return (
@@ -23,84 +23,85 @@ function FormComp({state}) {
       <section className="form--overlay">
         <Formik
           onSubmit={(values, actions) => {
-            actions.setSubmitting(false);
+            
             actions.resetForm();
-            setTimeout(()=>{  state(false)},1000)
+           
           }}
           validationSchema={validationSchema}
           initialValues={initialValues}
         >
           {(formik) => {
-          return (
-            <Form
-              noValidate
-              data-netlify-recaptcha="true"
-              name="contact"
-              className="grow"
-              method="POST"
-              data-netlify='true'
-              action="/contact"
-              onSubmit={formik.handleSubmit}
-            >
-              <input type="hidden" name="form-name" value="contact" />
-              <h3 className="sub">Contact us</h3>
-              <p>
-                <small className="text--small">*</small>
-                Required fields
-              </p>
-              <article className="form--control">
-                <label htmlFor="name" className="body">
-                  Name <small className="text--small">*</small>
-                </label>
-                <Field
-                  type="text"
-                  name="name"
-                  id="name"
-                  className={
-                    formik.touched.name && formik.errors.name ? "error" : ""
-                  }
-                />
-                <ErrorMessage name="name" component="small" />
-              </article>
-              <article className="form--control">
-                <label htmlFor="email" className="body">
-                  email <small className="text--small">*</small>
-                </label>
-                <Field
-                  type="email"
-                  name="email"
-                  id="email"
-                  className={
-                    formik.touched.email && formik.errors.email ? "error" : ""
-                  }
-                />
-                <ErrorMessage name="email" component="small" />
-              </article>
-              <article className="form--control">
-                <label htmlFor="comments" className="body">
-                  Comments
-                </label>
-                <Field as="textarea" name="comments" id="comments"></Field>
-              </article>
-              <article className="file--upload">
-                <Field type="file" name="file" id="file" hidden />
-                <button
-                  id="fileUpload"
-                  className="secondary-variant-2"
-                  type="file"
-                >
-                  upload files here
-                </button>
-              </article>
+            return (
+              <Form
+                noValidate
+                name="contact"
+                className="grow"
+                method="POST"
+                action="/contact"
+                onSubmit={formik.handleSubmit}
+              >
+                <input type="hidden" name="form-name" value="contact" />
+                <h3 className="sub">Contact us</h3>
+                <p>
+                  <small className="text--small">*</small>
+                  Required fields
+                </p>
+                <article className="form--control">
+                  <label htmlFor="name" className="body">
+                    Name <small className="text--small">*</small>
+                  </label>
+                  <Field
+                    type="text"
+                    name="name"
+                    id="name"
+                    className={
+                      formik.touched.name && formik.errors.name ? "error" : ""
+                    }
+                  />
+                  <ErrorMessage name="name" component="small" />
+                </article>
+                <article className="form--control">
+                  <label htmlFor="email" className="body">
+                    email <small className="text--small">*</small>
+                  </label>
+                  <Field
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={
+                      formik.touched.email && formik.errors.email ? "error" : ""
+                    }
+                  />
+                  <ErrorMessage name="email" component="small" />
+                </article>
+                <article className="form--control">
+                  <label htmlFor="comments" className="body">
+                    Comments
+                  </label>
+                  <Field as="textarea" name="comments" id="comments"></Field>
+                </article>
+                <article className="file--upload">
+                  <Field type="file" name="file" id="file" hidden />
+                  <button
+                    id="fileUpload"
+                    className="secondary-variant-2"
+                    type="file"
+                  >
+                    upload files here
+                  </button>
+                </article>
 
-              <article className="submit">
-                <button type="submit" className="primary-variant-2" id="submit">
-                  submit
-                </button>
-              </article>
-            </Form>
-          );
-         
+                <article className="submit">
+                  <button
+                    type="submit"
+                    className="primary-variant-2"
+                    id="submit"
+                  >
+                    submit
+                  </button>
+                </article>
+              </Form>
+            );
           }}
         </Formik>
       </section>
